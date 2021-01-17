97e870dc655f8953b8264d590e99d7f510b892f4
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2021-01-17 10:54:43 +0000

    clean SSRReflectionsMapComputePS.hlsl

--- SSRReflectionsMapComputePS.hlsl
+++ SSRReflectionsMapComputePS.hlsl
@@ -21,6 +21,9 @@ float3 GetFullViewPosition(float2 uv, float z)
 	float4 p1 = lerp(gFrustumFarCorner[1], gFrustumFarCorner[2], uv.x);
 	float3 ToFarPlane = lerp(p0.xyz, p1.xyz, 1 - uv.y);
 
+	// reconstruct full view space position (x,y,z)
+	// find t such that p = t*pin.tofarplane
+	// p.z = t*pin.tofarplane.z ==> t = p.z / pin.tofarplane.z
 	return (z / ToFarPlane.z) * ToFarPlane;
 }
 
@@ -30,7 +33,8 @@ float3 GetFullViewPosition(float2 uv)
 	return GetFullViewPosition(uv, NormalDepth.w);
 }
 
-// https://github.com/FlaxEngine/FlaxEngine/blob/master/Source/Shaders/SSR.shader
+// https://lettier.github.io/3d-game-shaders-for-beginners/screen-space-reflection.html
+// https://github.com/lettier/3d-game-shaders-for-beginners/blob/master/demonstration/shaders/fragment/screen-space-reflection.frag
 
 float4 main(VertexOut pin) : sv_target
 {
@@ -40,167 +44,133 @@ float4 main(VertexOut pin) : sv_target
 	float3 n = normaldepth.xyz;
 	float pz = normaldepth.w;
 
-	// reconstruct full view space position (x,y,z)
-	// find t such that p = t*pin.tofarplane
-	// p.z = t*pin.tofarplane.z ==> t = p.z / pin.tofarplane.z
 	//float3 p = (pz / pin.tofarplane.z) * pin.tofarplane;
 	float3 p = GetFullViewPosition(pin.TexCoord, pz);
 
+	float maxdistance = 100;
+	float resolution = 1;
+	int   steps = 10;
+	float thickness = 0.5f;
+
+	float2 texsize;
+	gNormalDepthMap.GetDimensions(texsize.x, texsize.y);
+	float2 texcoord = pin.PositionH.xy / texsize;
+
+	float3 positionfrom = p;
+	float3 positionfromunit = normalize(positionfrom);
+	float3 normal = normalize(n);
+	float3 pivot = normalize(reflect(positionfromunit, normal));
+
+	float3 positionto = positionfrom;
+	float4 uv = 0;
+
+	float3 viewstart = positionfrom + pivot * 0;
+	float3 viewend = positionfrom + pivot * maxdistance;
+
+	float4 fragstart = float4(viewstart, 1);
+	fragstart = mul(gProj, fragstart);
+	fragstart.xy /= fragstart.w;
+	fragstart.xy = fragstart.xy * float2(+0.5f, -0.5f) + 0.5f;
+	fragstart.xy *= texsize;
+
+	float4 fragend = float4(viewend, 1);
+	fragend = mul(gProj, fragend);
+	fragend.xy /= fragend.w;
+	fragend.xy = fragend.xy * float2(+0.5f, -0.5f) + 0.5f;
+	fragend.xy *= texsize;
+
+	float2 frag = fragstart.xy;
+	uv.xy = frag / texsize;
+
+	float deltax = fragend.x - fragstart.x;
+	float deltay = fragend.y - fragstart.y;
+	float usex = abs(deltax) >= abs(deltay) ? 1 : 0;
+	float delta = lerp(abs(deltay), abs(deltax), usex) * clamp(resolution, 0, 1);
+	float2 increment = float2(deltax, deltay) / max(delta, 0.001f);
+
+	float search0 = 0;
+	float search1 = 0;
+
+	int hit0 = 0;
+	int hit1 = 0;
+
+	float viewdistance = viewstart.z;
+	float depth = thickness;
+
+	for (int i = 0; i < int(delta); ++i)
 	{
-		float maxdistance = 100;
-		float resolution = 1;
-		int   steps = 100;
-		float thickness = 0.5f;
-
-		float2 texsize;
-		gNormalDepthMap.GetDimensions(texsize.x, texsize.y);
-		float2 texcoord = pin.PositionH.xy / texsize;
-
-		//return float4(texcoord, 0, 1);
-
-		float3 positionfrom = p;
-		float3 positionfromunit = normalize(positionfrom);
-		float3 normal = normalize(n);
-		float3 pivot = normalize(reflect(positionfromunit, normal));
-
-		float3 positionto = positionfrom;
-
-		//return float4(positionfrom, 1);
-		//return float4(positionfromunit, 1);
-		//return float4(normal, 1);
-		//return float4(pivot, 1);
-		//return float4(positionto, 1);
-
-		float4 uv = 0;
-
-		float3 viewstart = positionfrom + pivot * 0;
-		float3 viewend   = positionfrom + pivot * maxdistance;
-
-		float4 fragstart = float4(viewstart, 1);
-		fragstart = mul(gProj, fragstart);
-		fragstart.xy /= fragstart.w;
-		fragstart.xy = fragstart.xy * float2(+0.5f, -0.5f) + 0.5f;
-		//return float4(fragstart.xy, 0, 1);
-		fragstart.xy *= texsize;
-
-		float4 fragend = float4(viewend, 1);
-		fragend = mul(gProj, fragend);
-		fragend.xy /= fragend.w;
-		fragend.xy = fragend.xy * float2(+0.5f, -0.5f) + 0.5f;
-		//return float4(fragend.xy, 0, 1);
-		fragend.xy *= texsize;
-
-		float2 frag = fragstart.xy;
+		frag += increment;
 		uv.xy = frag / texsize;
 
-		float deltax = fragend.x - fragstart.x;
-		float deltay = fragend.y - fragstart.y;
-		float usex = abs(deltax) >= abs(deltay) ? 1 : 0;
-		float delta = lerp(abs(deltay), abs(deltax), usex) * clamp(resolution, 0, 1);
-		float2 increment = float2(deltax, deltay) / max(delta, 0.001f);
+		// do not sample outside the screen space
+		if (any(uv.xy < float2(0, 0)) || any(uv.xy > float2(1, 1))) break;
 
-		float search0 = 0;
-		float search1 = 0;
+		positionto = GetFullViewPosition(uv.xy);
 
-		int hit0 = 0;
-		int hit1 = 0;
+		search1 = lerp((frag.y - fragstart.y) / deltay, (frag.x - fragstart.x) / deltax, usex);
+		search1 = clamp(search1, 0, 1);
 
-		float viewdistance = viewstart.z;
-		float depth = thickness;
+		viewdistance = (viewstart.z * viewend.z) / lerp(viewend.z, viewstart.z, search1);
+		depth = viewdistance - positionto.z;
 
-		int j = 0;
-
-		for (int i = 0; i < int(delta); ++i)
+		if (depth > 0 && depth < thickness)
 		{
-			frag += increment;
-			uv.xy = frag / texsize;
-
-			if (any(uv.xy < float2(0, 0)) || any(uv.xy > float2(1, 1))) break;
-
-			positionto = GetFullViewPosition(uv.xy);
-
-			search1 = lerp((frag.y - fragstart.y) / deltay, (frag.x - fragstart.x) / deltax, usex);
-			search1 = clamp(search1, 0, 1);
-
-			viewdistance = (viewstart.z * viewend.z) / lerp(viewend.z, viewstart.z, search1);
-			//viewdistance = (viewstart.z * viewend.z) / lerp(viewstart.z, viewend.z, search1);
-			depth = viewdistance - positionto.z;
-
-			if (depth > 0 && depth < thickness)
-			{
-				hit0 = 1;
-				break;
-			}
-			else
-			{
-				search0 = search1;
-			}
-			
-			//if (++j >= 400) break;
+			hit0 = 1;
+			break;
 		}
-
-		//return float4(uv.xy, hit0, 1);
-
-		search1 = search0 + ((search1 - search0) / 2);
-
-		steps *= hit0;
-
-		//thickness = 1;
-
-		for (int i = 0; i < steps; ++i)
+		else
 		{
-			frag = lerp(fragstart.xy, fragend.xy, search1);
-			uv.xy = frag / texsize;
-
-			if (any(uv.xy < float2(0, 0)) || any(uv.xy > float2(1, 1))) break;
-
-			positionto = GetFullViewPosition(uv.xy);
-
-			viewdistance = (viewstart.z * viewend.z) / lerp(viewend.z, viewstart.z, search1);
-			depth = viewdistance - positionto.z;
-
-			if (depth > 0 && depth < thickness)
-			{
-				hit1 = 1;
-				search1 = search0 + ((search1 - search0) / 2);
-			}
-			else
-			{
-				float temp = search1;
-				search1 = search1 + ((search1 - search0) / 2);
-				search0 = temp;
-			}
+			search0 = search1;
 		}
-
-		//return float4(uv.xy, hit1, 1);
-
-		float visibility = hit1 *
-			(1 - max(dot(-positionfromunit, pivot), 0)) *
-			(1 - clamp(depth / thickness, 0, 1)) *
-			(1 - clamp(length(positionto - positionfrom) / maxdistance, 0, 1)) *
-			(uv.x < 0 || uv.x > 1 ? 0 : 1) *
-			(uv.y < 0 || uv.y > 1 ? 0 : 1);
-
-		visibility = saturate(visibility);
-
-
-		return float4(uv.xy, 0, visibility);
-
 	}
 
-	return float4(p, 1);
+	search1 = search0 + ((search1 - search0) / 2);
+
+	steps *= hit0;
+
+	for (int i = 0; i < steps; ++i)
+	{
+		frag = lerp(fragstart.xy, fragend.xy, search1);
+		uv.xy = frag / texsize;
+
+		// do not sample outside the screen space
+		if (any(uv.xy < float2(0, 0)) || any(uv.xy > float2(1, 1))) break;
+
+		positionto = GetFullViewPosition(uv.xy);
+
+		viewdistance = (viewstart.z * viewend.z) / lerp(viewend.z, viewstart.z, search1);
+		depth = viewdistance - positionto.z;
+
+		if (depth > 0 && depth < thickness)
+		{
+			hit1 = 1;
+			search1 = search0 + ((search1 - search0) / 2);
+		}
+		else
+		{
+			float temp = search1;
+			search1 = search1 + ((search1 - search0) / 2);
+			search0 = temp;
+		}
+	}
+
+	float visibility = hit1 *
+		(1 - max(dot(-positionfromunit, pivot), 0)) *
+		(1 - clamp(depth / thickness, 0, 1)) *
+		(1 - clamp(length(positionto - positionfrom) / maxdistance, 0, 1)) *
+		(uv.x < 0 || uv.x > 1 ? 0 : 1) *
+		(uv.y < 0 || uv.y > 1 ? 0 : 1);
+
+	return float4(uv.xy, 0, saturate(visibility));
 }
 
-struct RayPayload
-{
-	bool hit;
-	float2 TexCoord;
-};
+// https://virtexedgedesign.wordpress.com/2018/06/24/shader-series-basic-screen-space-reflections/
 
-// TODO
-// ray marching
-// binary search
-// blur
+//struct RayPayload
+//{
+//	bool hit;
+//	float2 TexCoord;
+//};
 
 //
 //float4 main(VertexOut pin) : SV_Target
@@ -213,15 +183,10 @@ struct RayPayload
 //
 //	float3 p = GetFullViewPosition(pin.TexCoord, pz);
 //
-//
 //	float3 dir = normalize(p);
 //	float3 ReflectDir = normalize(reflect(dir, normalize(n)));
 //
 //	float angle = clamp(dot(ReflectDir, normalize(n)), 0, 1);
-//
-//
-//
-//
 //
 //	float3 CurrPosition = 0;
 //	float3 CurrTexCoord = 0;
